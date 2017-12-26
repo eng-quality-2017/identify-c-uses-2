a4aa4e6845346253cb5115bffb8d71f4b44cef66
Aviram Dayan <aviram26@gmail.com>
2017-12-26 17:14:36 +0200

    fixed file separator to be portable

--- src/test/java/bgu/testing/identifyCuses2/TestGlobalCuses.java
+++ src/test/java/bgu/testing/identifyCuses2/TestGlobalCuses.java
@@ -2,14 +2,15 @@ package bgu.testing.identifyCuses2;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.util.ArrayList;
+
+import java.io.File;
 import java.util.List;
 
 public class TestGlobalCuses {
     @Test
     public void GolbalCuesesTest() {
         GlobalCuses globalCuss = new GlobalCuses();
-        List<String> allCusesForVar = globalCuss.findCUsesInMethod("src\\test\\resources\\CUsesInputNoConditions.java","c");
+        List<String> allCusesForVar = globalCuss.findCUsesInMethod("src" + File.separatorChar + "test" + File.separatorChar + "resources" + File.separatorChar + "CUsesInputNoConditions.java", "c");
         Assert.assertEquals(allCusesForVar.size(), 1);
     }
 
